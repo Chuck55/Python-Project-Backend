5912c8919a5cc8b2d9eb51ede1eaa760d682388e
Chuck55 <41636138+Chuck55@users.noreply.github.com>
2025-05-21 14:42:46 -0500

    pushing up fixes

--- api/src/api/__init__.py
+++ api/src/api/__init__.py
@@ -1,8 +1,7 @@
-# pylint: disable=missing-class-docstring
 from quart import Quart, request
-from pokemonService import validatePokemonExists
+from . import pokemonService
 from . import sql
-from quart_schema import QuartSchema, validate_request, validate_response
+from quart_schema import QuartSchema
 app = Quart(__name__)
 QuartSchema(app)
 
@@ -20,6 +19,7 @@ async def save_user_details():
 async def save_pokemon_details():
     schema = sql.PokemonSchema()
     result = schema.load(await request.get_json())
-    validatePokemonExists()
-    sql.insert_pokemon(result)
+    print(result)
+    pokemonService.validatePokemonExists(result["name"])
+    # sql.insert_pokemon(result)
     return "Got here"
--- api/src/api/pokemonService.py
+++ api/src/api/pokemonService.py
@@ -3,5 +3,6 @@ import requests
 
 def validatePokemonExists(pokemon):
     response = requests.get(
-        "https://pokeapi.co/api/v2/pokemon/" + pokemon, timeout=10)
+        "https://pokeapi.co/api/v2/pokemon/ditto", timeout=10)
+    print(response)
     return response.status_code == 200
